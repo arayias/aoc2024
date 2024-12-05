67bad72872c852b5d8027f6bc3d76b7f4706822e
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-12-05 17:07:42 +0000

    day 5

---
+++ 5.py
@@ -0,0 +1,79 @@
+from collections import deque
+from typing import Dict, Set, List
+from helper import read_file
+
+def generate_dependency_graph(rules: List[str]) -> Dict[int, Set[int]]:
+    dep : Dict[int,Set[int]] = {}
+    for rule in rules:
+        before, after = map(int,rule.split("|"))
+        if after not in dep:
+            dep[after] = set()
+        if before not in dep:
+            dep[before] = set()
+        dep[after].add(before)
+    return dep
+
+
+def solve_p1():
+    rules, updates = map(lambda x: x.split("\n"),read_file(__file__).split("\n\n"))
+    dep_dict = generate_dependency_graph(rules)
+    ans = 0
+    for update in updates:
+        seen : Set[int] = set()
+        current_pages = list(map(int,update.split(",")))
+        in_pages = set(current_pages)
+        valid = True
+        for page in current_pages:
+            requirements = (dep_dict[page].intersection(in_pages)).issubset(seen)
+            if not requirements:
+                valid = False
+                break
+            seen.add(page)
+        if not valid:
+            # reorder
+            continue
+        ans += current_pages[len(current_pages)//2]
+
+    return ans
+
+def solve_p2():
+    rules, updates = map(lambda x: x.split("\n"),read_file(__file__).split("\n\n"))
+    dep_dict = generate_dependency_graph(rules)
+    ans = 0
+    for update in updates:
+        seen : Set[int] = set()
+        current_pages = list(map(int,update.split(",")))
+        in_pages = set(current_pages)
+        valid = True
+        for page in current_pages:
+            requirements = (dep_dict[page].intersection(in_pages)).issubset(seen)
+            if not requirements:
+                valid = False
+                break
+            seen.add(page)
+        if not valid:
+            # current_pages.sort(key= lambda x : len(dep_dict[x].intersection(in_pages))) hacky but works doing it the right way
+            in_deg = {page : 0 for page in current_pages}
+            for page in current_pages:
+                for neighbor in dep_dict[page]:
+                    if neighbor in in_deg:
+                        in_deg[page] += 1
+            queue = deque([page for page in current_pages if in_deg[page] == 0])
+            correct_order : List[int]  = []
+            while queue:
+                curr = queue.popleft()
+                correct_order.append(curr)
+
+                for page in current_pages:
+                    if curr in dep_dict[page]:
+                        in_deg[page] -= 1
+                        if in_deg[page] == 0:
+                            queue.append(page)
+
+            ans += correct_order[len(correct_order) // 2]
+
+    return ans
+
+
+print(f"p1 : {solve_p1()}")
+print(f"p2 : {solve_p2()}")
