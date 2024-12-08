156fc2a461b9047e2190dd806e09b4aead401d15
Andreas Rayias <wowcaeu.andreas@gmail.com>
2024-12-08 04:59:22 +0000

    initial day 8 templ and templ.py

---
+++ templ.py
@@ -0,0 +1,15 @@
+from helper import read_file
+
+
+def solve_p1():
+    x = read_file(__file__).split('\n')
+    return -1
+
+
+def solve_p2():
+    x = read_file(__file__).split('\n')
+    return -1
+
+
+print(f"p1 : {solve_p1()}")
+print(f"p2 : {solve_p2()}")
